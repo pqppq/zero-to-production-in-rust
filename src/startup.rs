a20e2c82d8fe2fa46aa54b6112af1ca0a757b9d1
pqppq <yutatsu0119@gmail.com>
2023-04-08 17:19:43 +0900

    chore: add connection as app_data and insert query

--- src/startup.rs
+++ src/startup.rs
@@ -3,13 +3,16 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
     let server = HttpServer::new(|| {
         App::new()
             // web::get() is a shortcut for Route::new().guard(guard::Get())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection)
     })
     .listen(listener)?
     .run();
