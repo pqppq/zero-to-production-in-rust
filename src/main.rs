7da2d67c43d3102a47ea71a1b931a2a74f2f07f1
pqppq <yutatsu0119@gmail.com>
2023-04-09 18:20:00 +0900

    chore: update configurations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetory::{get_subscriber, init_subscriber};
@@ -13,11 +13,12 @@ async fn main() -> Result<(), std::io::Error> {
 
     // setup conection pool
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("127.0.0.1:{}", configuration.application.port);
     let listener = TcpListener::bind(address)?;
 
     run(listener, connection_pool)?.await?;
