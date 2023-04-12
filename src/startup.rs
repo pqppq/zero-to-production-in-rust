acf486e8106c1b74409af6c0d115fcde7384b57d
pqppq <yutatsu0119@gmail.com>
2023-04-12 21:26:30 +0900

    feat: add email client

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use std::net::TcpListener;
 
@@ -7,14 +8,16 @@ use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
