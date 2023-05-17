91d2ba3754417b5f8ed895d8703f7af58f01ab26
pqppq <yutatsu0119@gmail.com>
2023-05-17 20:44:53 +0900

    chore: add comment

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -9,7 +9,7 @@ async fn an_error_flash_message_is_set_on_failure() {
     // Arrange
     let app = spawn_app().await;
 
-    // Act
+    // Act - Try to login
     let login_body = serde_json::json!({
         "username": "username",
         "password": "password",
@@ -26,13 +26,13 @@ async fn an_error_flash_message_is_set_on_failure() {
     assert_is_redirect_to(&response, "/login");
     assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
 
-    // Act2
+    // Act2 - Follow the redirect
     let html_page = app.get_login_html().await;
 
     // Assert2
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 
-    // Act3
+    // Act3 - Reload the login page
     let html_page = app.get_login_html().await;
 
     // Assert3
