a2fb19f9886e0faa1c60313ca6508e00957ca6dd
pqppq <yutatsu0119@gmail.com>
2023-05-17 18:19:06 +0900

    chore: add hmac secret

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use secrecy::Secret;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{confirm, health_check, publish_newsletter, subscribe, home, login_form, login};
@@ -43,6 +44,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -66,11 +68,15 @@ pub async fn get_connection_pool(configuration: &DatabaseSettings) -> Result<PgP
 
 pub struct ApplicationBaseUrl(pub String);
 
+#[derive(Debug,Clone)]
+pub struct HmacSecret(pub Secret<String>);
+
 fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -88,6 +94,7 @@ fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
